4cbbf50ffb41e2fe200d6eb6fb0d5232795915d9
lai2-24 <lai2-24@itismeucci.com>
2024-12-10 14:10:19 +0100

    file html response

--- src/main/java/com/example/myThread.java
+++ src/main/java/com/example/myThread.java
@@ -2,7 +2,10 @@ package com.example;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
@@ -18,23 +21,54 @@ public class myThread extends Thread {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            String[] stringaTmp;
             String stringa;
+
+            String firstline = in.readLine();
+            String[] request = firstline.split(" ");
+
+            String method = request[0];
+            String resource = request[1];
+            String version = request[2];
+
             do {
                 stringa = in.readLine();
-                System.out.println(stringa + "\n");
+                
             } while (!stringa.isEmpty());
 
-            // String finale = "HTTP/1.1 404 Not found \r\n" + "Content-Lenght: 0 \r\n" +
-            // "\r\n";
-            // out.writeBytes(finale);
-            // s.close();
+            if (resource.equals("/index.html") || resource.equals("/file.txt") || resource.equals("/")) {
+            
+               // String responseBody = "Benvenuto nell'home pageeeee";
+                File file = new File("httdocs/index.html");
+                InputStream input = new FileInputStream(file);
 
-            String responseBody = "ciao a tutti";
-            out.writeBytes("HTTP/1.1  200 Ok\n");
-            out.writeBytes("Content-Type: text/plain \n");
-            out.writeBytes("Content-Length: " + responseBody.length() + "\n");
-            out.writeBytes("\n");
-            out.writeBytes(responseBody);
+                out.writeBytes("HTTP/1.1 200 Ok\n");
+                out.writeBytes("Content-Type: text/html\n");
+                out.writeBytes("Content-Length: " + file.length() + "\n");
+                out.writeBytes("\n");
+
+                byte[] buf = new byte[8192];
+                int n;
+                while((n = input.read(buf)) != -1 ){
+                    out.write(buf,0,n);
+                }
+                input.close();
+
+
+                //out.writebytes("responsebody")
+
+
+            } else {
+                String responseBody = "no smash";
+                out.writeBytes("HTTP/1.1 404 Not Found \n");
+                out.writeBytes("Content-Type: text/plain \n");
+                out.writeBytes("Content-Length: " + responseBody.length() + "\n");
+                out.writeBytes("\n");
+                out.writeBytes(responseBody);
+                s.close();
+            }
+
+           
 
 
             s.close();
